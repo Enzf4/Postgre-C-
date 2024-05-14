414261dfdde5973ab539b5c8b631ce75064e05d4
Enzo Farias <enzo.farias.r@gmail.com>
2024-05-14 18:24:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+let lastScrollTop = 0;
+let navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+if (scrollTop > lastScrollTop) {
+    navbar.classList.add('hide');
+} else {
+    navbar.classList.remove('hide');
+}
+
+lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
+
+
+let modal = document.getElementById('modal');
+let img = document.getElementById('img');
+let modalImg = document.getElementById("img01");
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    document.body.classList.add('disable-scroll');
+    
+}
+
+let span = document.getElementsByClassName("close")[0];
+let nav = document.querySelector('nav');
+
+span.onclick = function() {
+    modal.style.display = "none";
+    document.body.classList.remove('disable-scroll');
+    nav.style.display = 'flex';
+}
+
+let container = document.querySelector('.install-container');
+    images = container.querySelectorAll('img');
+
+
+images.forEach(img => {
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    document.body.classList.add('disable-scroll');
+    nav.style.display = 'none';
+}
+}); 
+
